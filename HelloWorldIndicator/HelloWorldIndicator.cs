c7a42cf6511c19cd038a075e49f4d6dbfdc3f59c
tmslpm <thomaslepomme@gmail.com>
2025-04-10 23:02:38 +0200

    first commit

---
+++ HelloWorldIndicator/HelloWorldIndicator.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+using TradingPlatform.BusinessLayer;
+
+namespace HelloWorldIndicator
+{
+    public class HelloWorldIndicator : Indicator
+    {
+        private Indicator _ema;
+
+        public HelloWorldIndicator()
+        {
+            Name = "My Indicator";
+            Description = "Hello World Indicator"; 
+            SeparateWindow = true;
+            _ema = null;
+
+            AddLineSeries("EMA5 50", Color.Magenta);
+        }
+
+        public override string ShortName => "HWI";
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+            
+            AddIndicator(_ema = Core.Indicators.BuiltIn.EMA(50, PriceType.Close));
+        }
+
+        protected override void OnUpdate(UpdateArgs args)
+        {
+            base.OnUpdate(args);
+            
+            SetValue(_ema.GetValue());
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            
+            _ema?.Dispose();
+            _ema = null;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Hello World Indicator Quantower
+
+# Setup
+
+1. Open the `.csproj` file: [HelloWorldIndicator/HelloWorldIndicator.csproj](./HelloWorldIndicator/HelloWorldIndicator.csproj)
+2. Update all references to Quantower libraries to match your installation path.
+   - In particular, ensure the reference to `TradingPlatform.BusinessLayer.dll` is correct.
+3. Verify that the `OutputPath` points to your Quantower `Scripts/Indicators` folder for both `Debug` and `Release` configurations.
+
+# Result
+
+![Hello World Indicator Quantower Preview Image](.github/assets/image.png)
